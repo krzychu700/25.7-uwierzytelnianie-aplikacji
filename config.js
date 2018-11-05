3718d00a52a9e878b074e776e7bd97486ffba626
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-05 18:19:42 +0100

    task finished

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '867651658124-qtih74f2kthuhnltqlbi8amh6694q71q.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'RgmsXQXFMAGJdYx5JONjGNZH',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
